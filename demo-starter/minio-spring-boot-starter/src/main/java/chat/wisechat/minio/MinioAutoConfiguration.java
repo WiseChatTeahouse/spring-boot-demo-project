0186b57d6d93715ac0226806c91db657459c0be5
siberia.hu <wisechatteahouse@gmail.com>
2025-01-21 17:37:47 +0800

    feat:加个条件注解

--- demo-starter/minio-spring-boot-starter/src/main/java/chat/wisechat/minio/MinioAutoConfiguration.java
+++ demo-starter/minio-spring-boot-starter/src/main/java/chat/wisechat/minio/MinioAutoConfiguration.java
@@ -3,6 +3,7 @@ package chat.wisechat.minio;
 import io.minio.MinioClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
@@ -21,11 +22,12 @@ public class MinioAutoConfiguration {
     private MinioProperties minioProperties;
 
     @Bean
+    @ConditionalOnMissingBean(MinioClient.class)
     public MinioClient minioClient() {
+        log.info("开始初始化Minio文件系统");
         return MinioClient.builder()
                 .endpoint(minioProperties.getUrl())
                 .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey()).build();
     }
-
 
 }
